fce201e9470e9e2e4b901f2dcebeed3c1028eb1f
Elleined <demadegu@gmail.com>
2024-01-23 00:10:22 +0800

    renamed project from server to api and added spring security dependency and added enabletransactionmanagement in main class

--- src/test/java/com/elleined/securityquestionapi/SecurityQuestionServerApplicationTests.java
+++ src/test/java/com/elleined/securityquestionapi/SecurityQuestionServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.elleined.securityquestionserver;
+package com.elleined.securityquestionapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
