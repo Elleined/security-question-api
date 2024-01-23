7784fddf2443a295c74a0f82bca37bb10bbed7fe
Elleined <demadegu@gmail.com>
2024-01-23 21:56:18 +0800

    added withname and withtype in userdto

--- src/main/java/com/elleined/securityquestionapi/dto/UserDTO.java
+++ src/main/java/com/elleined/securityquestionapi/dto/UserDTO.java
@@ -1,10 +1,12 @@
 package com.elleined.securityquestionapi.dto;
 
+import com.elleined.securityquestionapi.controller.QuestionController;
 import com.elleined.securityquestionapi.controller.SecurityQuestionController;
 import com.elleined.securityquestionapi.controller.UserController;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.HttpMethod;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -28,23 +30,44 @@ public class UserDTO extends RepresentationModel<UserDTO> implements HATEOUSLink
 
     @Override
     public UserDTO addSelfLinks() {
-        this.add(linkTo(methodOn(UserController.class).getById(id))
-                .withSelfRel()
-                .andAffordance(afford(methodOn(UserController.class).save(null))));
+        this.add(
+                linkTo(methodOn(UserController.class).getById(id))
+                        .withSelfRel()
+                        .withType("GET")
+                        .withTitle("Get by id")
+                        .andAffordance(afford(methodOn(UserController.class).save(null)))
+        );
         return this;
     }
 
     @Override
     public UserDTO addRelatedLinks() {
+        // Security Question
         this.add(
                 linkTo(methodOn(SecurityQuestionController.class).getAllByUser(id))
-                        .withRel("security-questions"),
+                        .withRel("security-questions")
+                        .withTitle("Get all current user security questions")
+                        .withType("GET"),
                 linkTo(methodOn(SecurityQuestionController.class).save(id, 0, null))
                         .withRel("security-questions")
                         .withType("POST")
                         .withTitle("Save security question"),
                 linkTo(methodOn(SecurityQuestionController.class).isAnswerCorrect(id, 0, null))
                         .withRel("security-questions")
+                        .withTitle("Check if security answer is correct")
+                        .withType("GET")
+        );
+
+        // Question
+        this.add(
+                linkTo(methodOn(QuestionController.class).save(null))
+                        .withRel("questions")
+                        .withTitle("Create question")
+                        .withType("POST"),
+                linkTo(methodOn(QuestionController.class).getAll())
+                        .withRel("questions")
+                        .withTitle("Get all question")
+                        .withType("GET")
         );
         return this;
     }
